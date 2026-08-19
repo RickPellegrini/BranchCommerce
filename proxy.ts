72143c22022396b487dbb80e969eff72bfceb654
Ricardo Pellegrini <pellegrini545@gmail.com>
2026-08-18 23:53:53 -0300

    Force Google account picker on sign in

--- proxy.ts
+++ proxy.ts
@@ -6,6 +6,7 @@ const isPublicRoute = createRouteMatcher([
   "/",
   "/sign-in(.*)",
   "/sign-up(.*)",
+  "/sso-callback(.*)",
   "/unauthorized(.*)",
   "/privacidade(.*)",
   "/excluir-conta(.*)",
